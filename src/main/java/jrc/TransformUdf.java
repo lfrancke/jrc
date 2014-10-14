38af111461aaca59fe225177db6eb996c17523de
Lars Francke <lars.francke@gmail.com>
2014-10-14 17:17:08 +0200

    Add a transform UDF

---
+++ src/main/java/jrc/TransformUdf.java
@@ -0,0 +1,55 @@
+package jrc;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+import com.vividsolutions.jts.io.WKBWriter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.io.BytesWritable;
+import org.geotools.geometry.jts.GeometryCoordinateSequenceTransformer;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+
+public class TransformUdf {
+
+  private static final Log LOG = LogFactory.getLog(CellUdf.class);
+
+  private final WKBReader reader = new WKBReader();
+  private final WKBWriter writer = new WKBWriter();
+  private GeometryCoordinateSequenceTransformer transformer;
+  private final BytesWritable transformedWritable = new BytesWritable();
+
+  public BytesWritable evaluate(String sourceCrsString, String targetCrsString, BytesWritable b)
+    throws HiveException, ParseException, FactoryException, TransformException {
+    if (b == null || b.getLength() == 0) {
+      LOG.warn("Argument is null or empty");
+      return null;
+    }
+
+    Geometry geometry = reader.read(b.getBytes());
+
+    if (geometry == null) {
+      LOG.warn("Geometry is null");
+      return null;
+    }
+
+    if (transformer == null) {
+      CoordinateReferenceSystem sourceCrs = CRS.decode(sourceCrsString);
+      CoordinateReferenceSystem targetCrs = CRS.decode(targetCrsString);
+      MathTransform mathTransform = CRS.findMathTransform(sourceCrs, targetCrs);
+      transformer = new GeometryCoordinateSequenceTransformer();
+      transformer.setMathTransform(mathTransform);
+    }
+
+    Geometry transformed = transformer.transform(geometry);
+    byte[] transformedBytes = writer.write(transformed);
+    transformedWritable.set(transformedBytes, 0, transformedBytes.length);
+    return transformedWritable;
+  }
+
+}
